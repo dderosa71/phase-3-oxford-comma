05354ff4be50bf6726a3159de74e132757aa4696
Dominic DeRosa <dderosa71@gmail.com>
2021-08-31 11:09:14 -0500

    commit tree b39b801ebfa8af9f873b624a92355f531c8f764e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        a = array[0,array.length-1].join(", ")
+        a << ", and #{array[array.length-1]}"
+    end
+    # array[0,array.length-1].join(', ')
 end
